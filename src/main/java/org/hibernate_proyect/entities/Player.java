c2a62fb6ba978e4915ded1e82a12caf018bca68c
MissyMogara <monicagara97@gmail.com>
2024-04-30 20:42:38 +0200

    First Commit

--- src/main/java/org/hibernate_proyect/entities/Player.java
+++ src/main/java/org/hibernate_proyect/entities/Player.java
@@ -8,7 +8,6 @@ import lombok.*;
 @Setter //Autogenerate Setters with lombok
 @AllArgsConstructor //Constructor with all args with lombok
 @NoArgsConstructor // Constructor without args with lombok
-@ToString // Method to String with lombok
 public class Player {
     //PROPERTIES
     @Id //Mark as ID
@@ -25,7 +24,7 @@ public class Player {
 
     private String language;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Game favorite_game;
 
     private String country;
@@ -39,5 +38,19 @@ public class Player {
         this.email = email;
         this.language = language;
         this.country = country;
+    }
+
+    //TO STRING
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Player{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", nick='").append(nick).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append(", language='").append(language).append('\'');
+        sb.append(", country='").append(country).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }
